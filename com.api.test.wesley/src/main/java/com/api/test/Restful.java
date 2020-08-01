9c88fc207340a6978f3659aec41453c8fb7810dc
Wesly-Yu <1633235633@qq.com>
2020-08-01 23:43:48 +0800

    封装restassured引擎2

--- com.api.test.wesley/src/main/java/com/api/test/Restful.java
+++ com.api.test.wesley/src/main/java/com/api/test/Restful.java
@@ -4,25 +4,21 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+
 import static io.restassured.RestAssured.given;
+
 import java.util.HashMap;
 
 public class Restful {
-    HashMap<String,Object> query = new HashMap<String, Object>();
+    HashMap<String, Object> query = new HashMap<String, Object>();
     public RequestSpecification requestSpecification;
-    public Response send(){
-        final RequestSpecification  requestSpecification = given().log().all();
-        query.entrySet().forEach( entry-> {
-            requestSpecification.queryParam(entry.getKey(),entry.getValue());
+
+    public static String template(String path, HashMap<String, Object> map) {
+        DocumentContext doucumentContext = JsonPath.parse(Restful.class.getResourceAsStream(path));
+        map.entrySet().forEach(entry -> {
+            doucumentContext.set(entry.getKey(), entry.getValue());
         });
-        return  requestSpecification.when().request("get","www.baidu.com");
-    }
-    public  static String  template(String path,HashMap<String,Object>map){
-        DocumentContext  doucumentContext = JsonPath.parse(Restful.class.getResourceAsStream(path));
-        map.entrySet().forEach(entry ->{
-            doucumentContext.set(entry.getKey(),entry.getValue());
-        } );
-        return  doucumentContext.jsonString();
+        return doucumentContext.jsonString();
 
 
     }
